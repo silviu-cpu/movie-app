e811a19bd299aa82a11e25f3589913e8aec4f7b5
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2022-09-04 14:25:42 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from './components/MovieList';
 
-function App() {
+const App = () => {
+  const [movies, setMovies ] = useState([{
+    "Title": "Star Wars",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode V - The Empire Strikes Back",
+    "Year": "1980",
+    "imdbID": "tt0080684",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode VI - Return of the Jedi",
+    "Year": "1983",
+    "imdbID": "tt0086190",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+}]);
+
+//fetch data from API
+const getMovieRequest = async () => {
+  const url = "http://www.omdbapi.com/?s=star wars&apikey=df17328e";
+
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  console.log(responseJson);
+}
+
+useEffect(() => { 
+  getMovieRequest();
+}, 
+[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='container-fluid movie-app'>
+    <div className='row'>
+    <MovieList movies = {movies} />
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
